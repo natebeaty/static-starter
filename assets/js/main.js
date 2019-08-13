6c0a4de5c099ee81b4a7359f29a4bda8822cc325
Nate Beaty <nate@clixel.com>
2019-08-13 13:26:29 -0400

    switch to div for breakpoint indicator, update scrollBody() and smoothScroll links

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,50 +6,71 @@
 
 var Main = (function($) {
 
-  var screen_width,
-      breakpoint_small = false,
-      breakpoint_medium = false,
-      breakpoint_large = false,
-      breakpoint_array = [480,1000,1200],
-      $document;
+  var breakpoints = [],
+      breakpointClasses = ['xl','lg','nav','md','sm','xs'],
+      $body,
+      $window,
+      $document,
+      headerOffset;
 
   function _init() {
     // Cache some common DOM queries
+    $body = $(document.body);
+    $window = $(window);
     $document = $(document);
 
-    // Add loaded class to body
-    $('body').addClass('loaded');
+    // DOM is loaded
+    $body.addClass('loaded');
 
-    // Set screen size vars
+    // Set breakpoint vars
     _resize();
 
+    // Zig zag the mothership
+    $window.on('resize', Main.resize);
+
     // Esc handlers
-    $(document).keyup(function(e) {
+    $document.keyup(function(e) {
       if (e.keyCode === 27) {}
     });
 
-    // Smoothscroll links
-    $('a.smoothscroll').click(function(e) {
-      e.preventDefault();
-      var href = $(this).attr('href');
-      _scrollBody($(href));
-    });
+    _initSmoothScrollLinks();
+    // _injectSvgDefs();
 
     // Scroll down to hash afer page load
-    $(window).load(function() {
-      if (window.location.hash) {
-        _scrollBody($(window.location.hash));
+    $window.on('load', function() {
+      if (window.location.hash && $(window.location.hash).length) {
+        _scrollBody(window.location.hash);
       }
     });
 
   }
 
   /**
+   * Smoothscroll links
+   */
+  function _initSmoothScrollLinks() {
+    $('a.smoothscroll,.smoothscroll a').click(function(e) {
+      var href = $(this).attr('href').replace(/(https?:)?\/\//,'');
+      var anchor = href.replace(/^[^#]/,'');
+      // Is this a link + an anchor?
+      if (href !== anchor) {
+        // Is this anchor on another page? Just return normal link behavior and location.hash will be handled on load
+        if (location.pathname !== href.replace(anchor,'')) {
+          return;
+        }
+      }
+      e.preventDefault();
+      _scrollBody(anchor);
+    });
+
+   }
+
+  /**
    * Pull in svg-defs.svg to body for <use> tags
    */
   function _injectSvgDefs() {
     var ajax = new XMLHttpRequest();
-    ajax.open('GET', 'assets/dist/svgs-defs.svg', true);
+    ajax.open('GET', '/assets/dist/svgs-defs.svg', true);
     ajax.send();
     ajax.onload = function(e) {
       var div = document.createElement('div');
@@ -60,14 +81,36 @@ var Main = (function($) {
   }
 
   /**
-   * Scroll body to element
+   * Scroll body to element using Velocity
    */
   function _scrollBody(element, duration, delay) {
-    element.velocity('scroll', {
+    // Defaults
+    if (typeof duration === 'undefined') {
+      duration = 500;
+    }
+    if (typeof delay === 'undefined') {
+      delay = 0;
+    }
+
+    // Sending '#hash' instead of jquery element?
+    if (typeof element === 'string') {
+      var anchor = element;
+      // Support for a[name=foo] anchors
+      element = $(anchor+',a[name="'+anchor.replace('#','')+'"]');
+      if (element.length === 0) {
+        return;
+      }
+    }
+
+    // Add a bit of breaking room to offset
+    var offset = 20 + headerOffset;
+
+    $(element).velocity('scroll', {
       duration: duration,
       delay: delay,
+      offset: -offset
     }, 'easeOutSine');
-  }
+   }
 
   /**
    * Track ajax pages in Analytics
@@ -87,10 +130,28 @@ var Main = (function($) {
    * Called in quick succession as window is resized
    */
   function _resize() {
-    screen_width = document.documentElement.clientWidth;
-    breakpoint_small = (screen_width > breakpoint_array[0]);
-    breakpoint_medium = (screen_width > breakpoint_array[1]);
-    breakpoint_large = (screen_width > breakpoint_array[2]);
+
+    // Check breakpoint indicator in DOM ( :after { content } is controlled by CSS media queries )
+    var breakpointIndicatorString = window.getComputedStyle(
+      document.querySelector('#breakpoint-indicator'), ':after'
+    ).getPropertyValue('content').replace(/['"]/g, '');
+    breakpoints = {};
+
+    // Check for breakpoint with `if (breakpoint['md'])`
+    for (var i = 0; i < breakpointClasses.length; i++) {
+      breakpoints[breakpointClasses[i]] = (breakpointIndicatorString === breakpointClasses[i] || (i>0 && breakpoints[breakpointClasses[i-1]]));
+    }
+
+    // Update header offset used in _scrollBody()
+    _setHeaderOffset();
+
+  }
+
+  /**
+   * Set headerOffset var used for scrollbody calculations
+   */
+  function _setHeaderOffset() {
+    headerOffset = 0; // Set to any sticky header height
   }
 
   /**
@@ -107,5 +168,6 @@ var Main = (function($) {
 })(jQuery);
 
 // Fire up the mothership
-jQuery(document).ready(Main.init);
-jQuery(window).resize(Main.resize);
+$(function() {
+  Main.init
+});
